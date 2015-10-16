50d1bf36b6679152346e231224047336e3e6d915
wheelin <greg.emry@gmail.com>
2015-10-16 12:28:57 +0200

    serie 1 interrupt completed

--- ser1_interrupt/skeleton.c
+++ ser1_interrupt/skeleton.c
@@ -4,6 +4,8 @@
 #include <linux/kernel.h>
 #include <linux/moduleparam.h>
 #include <linux/interrupt.h> 
+#include <linux/io.h>
+#include <linux/gpio.h>
 
 #define CHECK_RET(ret) if(ret != 0) return -1;
 
@@ -16,6 +18,7 @@ static int element_num = 1;
 module_param(element_num, int, 0);
 
 static int 		Pin_nr 			= 4;
+static int 		GPIO_nr			= {2,2,1,1};
 static int 		SW_pin_nr[4] 	= {5, 6, 6, 2};
 static int 		SW_io_nr[4] 	= {29, 30, 22, 18};
 static int 		SW_ID[4]		= {100, 101, 102, 103};
@@ -23,7 +26,7 @@ static char * 	SW_name[4] 		= {"SW1","SW2", "SW3","SW4"};
 
 irqreturn_t switch_irq_handler(int irq, void *dev_id)
 {
-	pr_info("%s has been pressed!!!", SW_name[(*dev_id) - 100]);
+	pr_info("Some switch has been pressed\n");
 	return IRQ_HANDLED;
 }
 
@@ -40,12 +43,12 @@ static int __init skeleton_init(void)
 	{
 		if(i < 2)
 		{
-			ret = gpio_request(EXYNOS5_GPX2(SW_pin_nr[i]));
-			ret = gpio_direction_imput(EXYNOS5_GPX2(SW_pin_nr[i]));
+			ret = gpio_request(EXYNOS5_GPX2(SW_pin_nr[i]), SW_name[i]);
+			ret = gpio_direction_input(EXYNOS5_GPX2(SW_pin_nr[i]));
 		} else
 		{
-			ret = gpio_request(EXYNOS5_GPX1(SW_pin_nr[i]));
-			ret = gpio_direction_imput(EXYNOS5_GPX1(SW_pin_nr[i]));	
+			ret = gpio_request(EXYNOS5_GPX1(SW_pin_nr[i]), SW_name[i]);
+			ret = gpio_direction_input(EXYNOS5_GPX1(SW_pin_nr[i]));	
 		}
 		CHECK_RET(ret);
 	}
@@ -54,10 +57,10 @@ static int __init skeleton_init(void)
 	for(i = 0; i < Pin_nr; i++)
 	{
 		ret = request_irq(gpio_to_irq(SW_io_nr[i]),
-							switch_irq_handler,
-							IRQF_SHARED|IRQF_TRIGGER_RISING,
-							SW_name[i],
-							&SW_ID[i]);
+			switch_irq_handler,
+			IRQF_SHARED|IRQF_TRIGGER_RISING,
+			SW_name[i],
+			&SW_ID[i]);
 		CHECK_RET(ret);
 	}
 	
@@ -70,7 +73,7 @@ static void __exit skeleton_exit(void)
 	int i;
 	pr_info("Freeing interrupts");
 	for(i = 0; i < Pin_nr; i++)
-		free_irq(gpio_to_irq(SW_io_nr[i]), SW_ID[i]);
+		free_irq(gpio_to_irq(SW_io_nr[i]), &SW_ID[i]);
 	pr_info("Interrupts freed");
 }
 
