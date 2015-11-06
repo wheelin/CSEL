fa9b2bb42b1dd324162d02d573273522d1933374
emilie gsponer <emilie.gsponer@gmail.com>
2015-11-06 12:32:01 +0100

    Finish exercice 6

--- PilotesPeripheriques/exercice2-mmapModule/noyau/skeleton.c
+++ PilotesPeripheriques/exercice2-mmapModule/noyau/skeleton.c
@@ -9,10 +9,6 @@
 
 static int major = 0; /* Major number */
 
-#define BUF_SIZE 64
-static char buffer[BUF_SIZE];
-static size_t num = 0; /* Number of available bytes in the buffer */
-
 void vma_open(struct vm_area_struct *vma);
 void vma_close(struct vm_area_struct *vma);
 
@@ -20,31 +16,6 @@ static struct vm_operations_struct remap_vma_ops = {
 	.open =  vma_open,
 	.close = vma_close,
 };
-
-static ssize_t mod_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
-{
-	size_t real;
-	real = min((size_t)BUF_SIZE, count);
-	if (real)
-		if (copy_from_user(buffer, buf, real))
-			return -EFAULT;
-	num = real; /* Destructive write (overwrite previous data if any) */
-	pr_info("mod: write %s\n", buffer);
-	return real;
-}
-
-static ssize_t mod_read(struct file *file, char *buf, size_t count,
-loff_t *ppos)
-{
-	size_t real;
-	real = min(num, count);
-	if (real)
-		if (copy_to_user(buf, buffer, real))
-			return -EFAULT;
-	num = 0; /* Destructive read (no more data after a read) */
-	pr_info("mod: read %s\n", buffer);
-	return real;
-}
 
 static int mod_open(struct inode *inode, struct file *file)
 {
@@ -90,8 +61,6 @@ static int mod_release(struct inode *inode, struct file *file)
 
 static struct file_operations mod_fops = {
 	.owner = THIS_MODULE,
-	.read = mod_read,
-	.write = mod_write,
 	.open = mod_open,
 	.release = mod_release,
 	.mmap = mod_mmap,
@@ -121,6 +90,6 @@ module_init(mod_init);
 module_exit(mod_exit);
 
 MODULE_AUTHOR("Emilie Gsponer");
-MODULE_DESCRIPTION("drive oriented character");
+MODULE_DESCRIPTION("driver oriented character");
 MODULE_LICENSE("GPL");
 
